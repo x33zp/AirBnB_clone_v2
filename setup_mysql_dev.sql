e8810174fb736ef7ce2f795052c670d7def2b86a
x33zp <zubbypeculiar@gmail.com>
2024-06-25 07:14:39 -0700

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,6 @@
+-- Creates a database 'hbnb_dev_db', a new user 'hbnb_dev' that has all priviledges on the database.
+-- and select priviledges on the database 'performance_schema'
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';

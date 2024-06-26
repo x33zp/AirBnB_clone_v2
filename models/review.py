6e2476511b4a5f959daa748d452d299a6d3b0118
x33zp <zubbypeculiar@gmail.com>
2024-06-26 15:59:03 -0700

    modified

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""Review module.
+
+This module which inherits from BaseModel defines the State
+class, which represents the reviews given by the User.
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """This class represents Review."""
+    __tablename__ = 'reviews'
+
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)

5ffcb8a0875ff540ff9704dc134fe9baba2ef113
x33zp <zubbypeculiar@gmail.com>
2024-07-13 00:07:21 -0700

    cp fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,80 +1,57 @@
 #!/usr/bin/python3
-"""Fabric script that creates and distributes an archive to your web servers
-"""
-from fabric.api import env, run, put, local
-from os import path
-from time import strftime
-from datetime import date
+# A Fabric script that generates a .tgz archive
+from datetime import datetime
+from fabric.api import local, env, run, put
+import tarfile
+import os.path
 
+
+env.hosts = ['54.234.63.33', '18.234.193.44']
 env.user = 'ubuntu'
-env.hosts = ['100.25.180.67', '54.210.195.91']
+env.key_filename = '~/.ssh/school'
 
 
 def do_pack():
-    """A script that generates archive the contents of web_static folder"""
-
-    dt = strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(dt)
-
-    try:
-        if path.isdir("versions") is False:
-            local("mkdir versions")
-
-        local("tar -czvf {} web_static".format(file_name))
-
-        return file_name
-
-    except Exception as e:
+    """ Creates a tar gzipped archive of the directory web_static """
+    d = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(d.year,
+                                                         d.month,
+                                                         d.day,
+                                                         d.hour,
+                                                         d.minute,
+                                                         d.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").format is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
 
 
 def do_deploy(archive_path):
-    """A script that distributes an archive"""
+    """ Distributes an archive to a web server """
+    fd = archive_path.split('/')[1]
     try:
-        archive_file = archive_path.split('/')[1]
-        file_name = archive_file.split('.')[0]
-
-        # Upload the archive file
-        put(archive_path, '/tmp/{}'.format(archive_file))
-
-        # Create the folder where the archive will be uncompressed
-        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-
-        # Uncompress the archive to the folder 'file_name'
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
-            .format(archive_file, file_name))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_file))
-
-        # move contents into host web_static
+        put(archive_path, '/tmp/{}'.format(fd))
+        run('mkdir -p /data/web_static/releases/{}'.format(fd))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(fd, fd))
+        run('rm /tmp/{}'.format(fd))
         run('mv /data/web_static/releases/{}/web_static/*\
-                /data/web_static/releases/{}/'.format(file_name, file_name))
-
-        # remove extraneous web_static dir
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-
-        # Delete the symbolic link /data/web_static/current from the web server
+        /data/web_static/releases/{}/'.format(fd, fd))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(fd))
         run('rm -rf /data/web_static/current')
-
-        # Create a new the symbolic link
         run('ln -s /data/web_static/releases/{}/\
-                /data/web_static/current'.format(file_name))
-
+        /data/web_static/current'.format(fd))
         print("New version deployed!")
         return True
-
     except Exception:
         print("New version not deployed!")
         return False
 
 
 def deploy():
-    """Automation"""
+    """ Automates Everything """
     archive = do_pack()
-
     if archive is None:
         return False
-
     return do_deploy(archive)

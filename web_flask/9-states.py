0a49477c42478c91bf5757ff27de5c1d5a68e116
x33zp <zubbypeculiar@gmail.com>
2024-08-08 02:04:13 +0100

    states.cities

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -20,3 +20,13 @@ def states_id(id):
         if state.id == id:
             return render_template('9-states.html', state=state)
     return render_template('9-states.html')
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ Removes current SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

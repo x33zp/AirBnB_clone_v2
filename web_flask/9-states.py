cfc77991c0f99ccfbaa0a4e5a9de0ac6ba26bdc9
x33zp <zubbypeculiar@gmail.com>
2024-08-08 01:59:13 +0100

    states.cities

---
+++ web_flask/9-states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Importing Flask to run the web app
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Render state_list html page to display States created"""
+    state = storage.all()
+    return render_template('9-states.html', states=states)
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays list of States with info about 'id' if it's present"""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
